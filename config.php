febbaaf15486feaaafabdd742900ab5cab3f97b9
Mike Northorp <mike.northorp@gmail.com>
2014-03-04 23:07:01 -0400

    Config values for localhost added

--- config.php
+++ config.php
@@ -3,12 +3,12 @@
 /* The following variables are used to store config information for the database. */
 
 // Hostname for the database
-$db_host = 'wqdf';
+$db_host = 'localhost';
 // Username for database
-$db_user = '';
+$db_user = 'root';
 // Password for database					
 $db_pass = '';		
 // Name of database connecting too			
-$db_name = '';   
+$db_name = 'hangman';   
 
 ?>
