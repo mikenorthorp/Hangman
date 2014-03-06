4aae42bdd967d9a676ce01c5a9320f955c693ae7
Mike Northorp <mike.northorp@gmail.com>
2014-03-06 15:52:41 -0400

    Added more checks for word, and tracking wins and losses

--- index.php
+++ index.php
@@ -33,12 +33,14 @@ $isReset = 0;
 // Set username to whatever is stored in the session
 $username = $_SESSION['username'];
 
-// Set the word to whatever is stored in the session
-$word = $_SESSION['word'];
+// Set the word to whatever is stored in the session if it is set
+if(isset($_SESSION['word'])) {
+	$word = $_SESSION['word'];
+}
 
 $gameInProgress = 0;
 // Check if game is in progress 
-if($_SESSION['inProgress'] != 0) {
+if(isset($_SESSION['inProgress'])) {
 	$gameInProgress = $_SESSION['inProgress'];
 }
 
@@ -53,12 +55,15 @@ if($username == "admin") {
 
 $numFailedGuess = 0;
 // Check the number of letters guessed
-if($_SESSION['numFailedGuess'] != 0) {
+if(isset($_SESSION['numFailedGuess'])) {
 	$numFailedGuess = $_SESSION['numFailedGuess'];
 }
 
 // This gets written too if there are any errors and displayed on the page.
 $gameError = "";
+
+// This gets written if there is a win or loss message for the user
+$gameMessage = "";
 
 /* -------------------------- */
 /*   END VARIABLE SETUP 	  */
@@ -80,30 +85,40 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		$result = mysqli_query($link, $query);
 		$numRows = mysqli_num_rows($result);
 
+		// Get a random word from the list to grab from
+		$wordIndex = rand(1, $numRows);
+
 		// Check if any words are returned
 		if ($numRows == 0) {
 			$gameError = "No words availible, please login as Admin and upload some words.";
-			// Alerts the user that no words are availbile via an alert popup
-			echo "<script type='text/javascript'>alert('$gameError');</script>";
 
 			// Turn the game off
 			$_SESSION['inProgress'] = 0;
 			$gameInProgress = $_SESSION['inProgress'];
 		} else {
 			// Grab a word random word from database and set it
+			$query = "SELECT word FROM words WHERE id=" . $wordIndex;
+			$result = mysqli_query($link, $query);
+			$row = mysqli_fetch_assoc($result);
 
-
-			// Grab a random word from a random row returned
-			$word = "Test";
+			// Grab the word from the random unique ID picked
+			$word = $row['word'];
 
 			// Set the session variable for the word
 			$_SESSION['word'] = $word;
+
+			// Get length of word
+			$_SESSION['wordLength'] = strlen($word);
+
+			// Set up an empty array with all spaces in place of word
+			$_SESSION['hiddenWord'] = array_fill(0, $_SESSION['wordLength'], '_');
 		}
 	}
 
 	// Reset the game if the user chooses too
 	if(isset($_POST['reset'])) {
 		$isReset = 1;
+		$isLoss = 1;
 	}
 
 	// If a word list is uploaded
@@ -115,9 +130,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	    	$query = "DELETE FROM words";
 			mysqli_query($link, $query);
 
+			// Reset auto increment
+			$query = "ALTER TABLE words AUTO_INCREMENT = 1";
+			mysqli_query($link, $query);
+			
 		    while (($line = fgets($handle)) !== false) {
 		        // Upload each word to the database
 		        $line = preg_replace('/\s+/', '', $line);
+		        $line = strtoupper($line);
 				$query = "INSERT INTO words (word) VALUES ('" . mysqli_real_escape_string($link, $line) . "')";
 				mysqli_query($link, $query);
 		    }
@@ -125,8 +145,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	}
 }
 
+// Check if game over and dont do following
+$gameOver = 0;
+if(isset($_SESSION['gameOver'])) {
+	if ($_SESSION['gameOver'] == 1) {
+		$gameOver = 1;
+	}
+}
 // Check if a letter is guessed only if game is in progress (stop people from running GET after game over)
-if ($_SERVER["REQUEST_METHOD"] == "GET" && $gameInProgress == 1) {
+if ($_SERVER["REQUEST_METHOD"] == "GET" && $gameInProgress == 1 && $gameOver == 0) {
 	$alpha = "";
 	if(isset($_GET['guess'])) {
 		$alpha = $_GET['guess'];
@@ -137,24 +164,37 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && $gameInProgress == 1) {
 		// If letter is not in word, count as incorrect guess.
 		if (strpos($word, $alpha) === false) {
 			$correct = 0;
+		} else {
+			// If letter is in word, loop through full word and fill in value where it should go
+			for($i = 0; $i < $_SESSION['wordLength']; $i++) {
+				if($word[$i] == $alpha) {
+					$_SESSION['hiddenWord'][$i] = $word[$i];
+				}	
+			}
 		}
 
 		// If not in word increase fails
 		if ($correct != 1) {
 			$numFailedGuess++;
 			$_SESSION['numFailedGuess'] = $numFailedGuess;	
-		}	
+		} else {
+			// Check for win condition if guess is correct by checking for _ in hidden word
+			if (strpos(implode("", $_SESSION['hiddenWord']), '_') === false) {
+				$isWin = 1;
+				$_SESSION['isWin'] = 1;
+				$_SESSION['gameOver'] = 1;
+				$gameMessage = "You have won! Hit reset to play again and see if you are on the highscore board!";
+			}
+		}
 
 		// Check if they failed their last guess 
 		if($numFailedGuess == 5) {
-			echo "Test";
 			// Set as a loss for user
 			$isLoss = 1;
-			// Reset session except for the user
-			$isReset = 1;
+			$_SESSION['isLoss'] = 1;
+			$_SESSION['gameOver'] = 1;
 			// Display a message telling the user they lost
-			$message = "You have lost!";
-			echo "<script type='text/javascript'>alert('$message');</script>";
+			$gameMessage = "You have lost! Hit reset to play again!";
 		} 
 	}
 }
@@ -168,8 +208,27 @@ if($isReset == 1) {
 	$_SESSION['inProgress'] = 0;
 	$gameInProgress = 0;
 
-	// Set as a loss
-	$isLoss = 1;
+	// Set game over to not over
+	$_SESSION['gameOver'] = 0;
+
+	// Check if resetting after a win, so dont count as loss
+	if(isset($_SESSION['isWin'])) {
+		// No win detected so reset is because of loss or just a reset press
+		if($_SESSION['isWin'] != 1) {
+			$isLoss = 1;
+		}
+		// Reset win condition detected so don't count as loss and reset win to 0
+		$_SESSION['isWin'] = 0;
+	} else {
+		// There is a loss an no win has happened yet
+		$isLoss = 1;
+	}
+
+	// Reset loss condition as well if set
+	if(isset($_SESSION['isLoss'])) {
+		$_SESSION['isLoss'] = 0;
+	}
+
 }
 
 // If a loss is detected add to losses
@@ -222,7 +281,7 @@ if($isWin == 1) {
 <div id="content">
 	<h1> INFX 2670 Assignment 3 - Michael Northorp </h1>
 	<!-- This displays the word randomly chosen if the game is started -->
-	<?php if($gameInProgress == 1) : ?>
+	<?php if($gameInProgress === 1) : ?>
 	<div id="word_display">
 		<p> The word to guess is <?php echo $word; ?> </p>
 	</div>
@@ -248,6 +307,7 @@ if($isWin == 1) {
 		<!-- This triggers the start of the game, and is hidden if the game is started -->
 		<?php if($gameInProgress != 1) : ?>
 			<div id="game_trigger">
+				<br><span class="error"><?php echo $gameError; ?></span>
 				<form method="post">
 					<input type="hidden" name="start" value="start"/>
 					<input type="submit" value="Play Hangman" id="btn">
@@ -257,12 +317,22 @@ if($isWin == 1) {
 		<!-- Generate a list of links for alphabet guessing for hangman if game is on -->
 		<?php if($gameInProgress == 1) : ?>
 		<div id="hangman_image">
-			<img src="images/hang<?php echo $numFailedGuess ?>.gif">
+			<img src="images/hang<?php echo $numFailedGuess; ?>.gif">
+			<br><span class="game_message"><?php echo $gameMessage; ?></span>
 		</div>
 
 		<!-- This displays the guessed word when correct letters are guessed, as well as blank spaces at the start -->
 		<div id="guessed_word">
-
+		<?php
+			// Display the hidden word with same length as word to guess
+			// But has underscores if letter is not guessed
+			$hiddenWord = $_SESSION['hiddenWord'];
+			echo "<p>";
+			for($i = 0; $i < $_SESSION['wordLength']; $i++) {
+				echo "$hiddenWord[$i] ";
+			}
+			echo "</p>";
+		?>
 		</div>
 		<div id="alpha_list">
 			<p> Select a letter below to fill in the word above </p>
